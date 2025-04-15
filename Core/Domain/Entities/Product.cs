df4a738ce10003de3f1dc17a781ef97549898385
Helal <hazemhelal.se@gmail.com>
2025-04-15 14:17:46 +0200

    Product - ProductType - ProductBrand Entities

---
+++ Core/Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Entities
+{
+    public class Product: BaseEntity<int>
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+
+        // Navigation Properties
+        public int TypeId { get; set; }
+        public int BrandId { get; set; }
+
+        public ProductBrand ProductBrand { get; set; } = default!;
+        public ProductType ProductType { get; set; } = default!;
+    }
+}
---
+++ Core/Domain/Entities/ProductType.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class ProductType: BaseEntity<int>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public virtual ICollection<Product> Products { get; set; } = new HashSet<Product>();
+    }
+}
