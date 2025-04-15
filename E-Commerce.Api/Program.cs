f766d0cbd10e8ed213dce5eca50ce121cea38d43
Helal <hazemhelal.se@gmail.com>
2025-04-15 14:46:07 +0200

    DbContext - Configurations

--- E-Commerce.Api/Program.cs
+++ E-Commerce.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
 
 namespace E_Commerce.Api
 {
@@ -13,7 +15,8 @@ namespace E_Commerce.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
