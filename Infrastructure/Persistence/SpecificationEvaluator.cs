1c0d0738da9a3fcc6604dd065014385105aabd8b
Helal <hazemhelal.se@gmail.com>
2025-04-25 19:38:05 +0300

    SpecificationsEvalutor

---
+++ Infrastructure/Persistence/SpecificationEvaluator.cs
@@ -0,0 +1,21 @@
+﻿namespace Persistence
+{
+    internal static class SpecificationEvaluator
+    {
+        public static IQueryable<TEntity> GetQuery<TEntity, TKey>(IQueryable<TEntity> inputQuery, ISpecification<TEntity, TKey> specification)
+            where TEntity : BaseEntity<TKey>
+        {
+            var query = inputQuery;
+            if (specification.Criteria != null)
+            {
+                query = query.Where(specification.Criteria);
+            }
+            //foreach (var include in specification.Includes)
+            //{
+            //    query = query.Include(include);
+            //}
+            query = specification.Includes.Aggregate(query, (current, include) => current.Include(include));
+            return query;
+        }
+    }
+}
